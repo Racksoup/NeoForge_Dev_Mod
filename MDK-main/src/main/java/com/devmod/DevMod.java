fdd680e8a18ad54809ef7c520bf572222a73194b
racksoup <rack.connor1@gmail.com>
2024-06-24 08:44:40 -0400

    button added to inventory menu correctly

--- MDK-main/src/main/java/com/devmod/DevMod.java
+++ MDK-main/src/main/java/com/devmod/DevMod.java
@@ -1,15 +1,24 @@
 package com.devmod;
 
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.ImageButton;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.InventoryScreen;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.animal.Turtle;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.neoforged.neoforge.client.event.InputEvent;
+import net.neoforged.neoforge.client.event.ScreenEvent;
 import net.neoforged.neoforge.common.NeoForge;
 import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -28,11 +37,15 @@ import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.neoforge.event.server.ServerStartingEvent;
 
+import java.util.function.Supplier;
+
 @Mod(DevMod.MODID)
 public class DevMod
 {
     public static final String MODID = "dev_mod";
     public static final Logger LOGGER = LogUtils.getLogger();
+
+
 
     public DevMod(IEventBus modEventBus, ModContainer modContainer)
     {
@@ -43,14 +56,37 @@ public class DevMod
         ModTab.CREATIVE_MODE_TABS.register(modEventBus);
         NeoForge.EVENT_BUS.addListener(DevMod::blockBreak);
         NeoForge.EVENT_BUS.addListener(DevMod::onEntityInteract);
-        NeoForge.EVENT_BUS.addListener(DevMod::onPlayerInteract);
+//        NeoForge.EVENT_BUS.addListener(DevMod::onPlayerInteract);
+        NeoForge.EVENT_BUS.addListener(DevMod::onScreenInit);
+        NeoForge.EVENT_BUS.addListener(DevMod::onScreenRender);
     }
 
-    public static void onPlayerInteract(PlayerInteractEvent.RightClickBlock event) {
-        if (event.getEntity().level().isClientSide) {
-            Minecraft minecraft = Minecraft.getInstance();
-            Player player = minecraft.player;
-            minecraft.setScreen(new ModInventoryScreen(player));
+//    public static void onPlayerInteract(InputEvent.Key event) {
+//        if (event.getKey() == GLFW.GLFW_KEY_E && event.getAction() == GLFW.GLFW_PRESS) {
+//            Minecraft minecraft = Minecraft.getInstance();
+//            Player player = minecraft.player;
+//            minecraft.setScreen(new ModInventoryScreen(player));
+//
+//
+//        }
+//    }
+
+    public static void onScreenInit(ScreenEvent.Init.Post event) {
+        Screen screen = event.getScreen();
+
+        if (screen instanceof InventoryScreen inv) {
+            event.addListener(new DevButton(inv.getGuiLeft() + 126, inv.height / 2 - 22, 20, 18));
+        }
+    }
+
+    public static void onScreenRender(ScreenEvent.Render.Post event) {
+        Screen screen = event.getScreen();
+        if (screen instanceof InventoryScreen inv) {
+            inv.renderables.forEach(widget -> {
+                if (widget instanceof DevButton button) {
+                    button.setPosition(20, 20);
+                }
+            });
         }
     }
 
@@ -110,4 +146,27 @@ public class DevMod
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
     }
+
+
+    public static class DevButton extends Button
+    {
+        public DevButton(int pX, int pY, int pWidth, int pHeight) {
+            super(pX, pY, pWidth, pHeight, Component.literal("faer"), button -> {
+                DevMod.LOGGER.info("new BUtton");
+            }, pMessageSupplier -> {
+                // Custom narration message logic
+                MutableComponent originalMessage = pMessageSupplier.get();
+                // Modify the original message or create a new one
+                MutableComponent customMessage = originalMessage.copy().append(" - Custom Narration");
+                return customMessage;
+            });
+        }
+//
+//        public static Button btn = Button.builder(Component.literal("ff"), button -> {
+//            DevMod.LOGGER.info("ffffff");
+//        }).build();
+
+    }
 }
+
+
