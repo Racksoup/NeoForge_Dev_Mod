96b6cb52e2ab62caaf6bc60bb6196a327c9b6f0e
racksoup <rack.connor1@gmail.com>
2024-06-28 08:37:43 -0400

    textured button added to modcraftingscreen

---
+++ MDK-main/src/main/java/com/devmod/ModCraftingScreenButton.java
@@ -0,0 +1,27 @@
+package com.devmod;
+
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+
+
+public class ModCraftingScreenButton extends Button {
+    private static final ResourceLocation BUTTON_TEXTURE = ResourceLocation.fromNamespaceAndPath(DevMod.MODID, "textures/buttons/mod_crafting_screen_button.png");
+
+    public ModCraftingScreenButton(int x, int y, int width, int height) {
+        super(x, y, width, height, Component.empty(),
+                button -> {
+                    DevMod.LOGGER.info("ModCraftingScreenButton");
+                }, Button.DEFAULT_NARRATION);
+    }
+
+    @Override
+    public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks) {
+        if (isMouseOver(mouseX, mouseY)) {
+            guiGraphics.blit(BUTTON_TEXTURE, getX(), getY(),0, 19, getWidth(), getHeight());
+        } else {
+            guiGraphics.blit(BUTTON_TEXTURE, getX(), getY(),0, 0, getWidth(), getHeight());
+        }
+    }
+}
