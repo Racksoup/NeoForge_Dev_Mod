833dfe0443afb607808bc57456b1279950363521
racksoup <rack.connor1@gmail.com>
2024-06-29 01:51:10 -0400

    imagebutton added to modcraftingscreen, minecraft one removed

--- MDK-main/src/main/java/com/devmod/ModCraftingScreen.java
+++ MDK-main/src/main/java/com/devmod/ModCraftingScreen.java
@@ -1,16 +1,16 @@
 package com.devmod;
 
-import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.ImageButton;
 import net.minecraft.client.gui.screens.inventory.CraftingScreen;
+import net.minecraft.client.gui.screens.recipebook.RecipeBookComponent;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.CraftingMenu;
 
 
 public class ModCraftingScreen extends CraftingScreen {
-    private Button customButton;
-    private boolean wasRecipeBookVisible;
     private ModCraftingScreenButton modButton;
+    private ImageButton imageButton;
 
     public ModCraftingScreen(CraftingMenu pMenu, Inventory pPlayerInventory) {
         super(pMenu, pPlayerInventory, Component.literal("ModCrafting"));
@@ -21,47 +21,44 @@ public class ModCraftingScreen extends CraftingScreen {
     protected void init() {
         DevMod.LOGGER.info("ModCraftingScreen init");
         super.init();
+
+        for (var widget : this.renderables) {
+            if (widget instanceof ImageButton) {
+                imageButton = (ImageButton) widget;
+                break;
+            }
+        }
+        this.renderables.remove(imageButton);
+        this.children().remove(imageButton);
+
         int x = this.leftPos;
         int y = this.topPos;
-        customButton = Button.builder(Component.literal("Ride The Lightning!"), button -> {
-            DevMod.LOGGER.info("Button Clicked!");
-        })
-            .bounds(x, y, 100, 20)
-            .build();
-        this.addRenderableWidget(customButton);
-
-        modButton = new ModCraftingScreenButton(x + 10, y + 10, 20, 18);
+        modButton = new ModCraftingScreenButton(x + 5, y + 34, 20, 18);
         this.addRenderableWidget(modButton);
     }
 
     @Override
     public void containerTick() {
         super.containerTick();
-
-        boolean isRecipeBookVisible = this.getRecipeBookComponent().isVisible();
-        if (isRecipeBookVisible != this.wasRecipeBookVisible) {
-            this.updateButtonPosition();
-            this.wasRecipeBookVisible = isRecipeBookVisible;
-        }
     }
 
-    @Override
-    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
-        boolean result = super.mouseClicked(mouseX, mouseY, mouseButton);
-
-        if (this.getRecipeBookComponent().mouseClicked(mouseX, mouseY, mouseButton)) {
-            DevMod.LOGGER.info("Recipe book interaction detected!");
-            return true;
-        }
-
-        return result;
-    }
+//    @Override
+//    public boolean mouseClicked(double mouseX, double mouseY, int mouseButton) {
+//        boolean result = super.mouseClicked(mouseX, mouseY, mouseButton);
+//
+//        if (this.getRecipeBookComponent().mouseClicked(mouseX, mouseY, mouseButton)) {
+//            DevMod.LOGGER.info("Recipe book interaction detected!");
+//            return true;
+//        }
+//
+//        return result;
+//    }
 
     private void updateButtonPosition() {
         int xPos = this.leftPos;
         int yPos = this.topPos;
 
-        this.customButton.setX(xPos);
-        this.customButton.setY(yPos);
+        this.modButton.setX(xPos + 10);
+        this.modButton.setY(yPos + 10);
     }
 }
