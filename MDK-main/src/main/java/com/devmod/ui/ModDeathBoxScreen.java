b02870a2fc50759be2fcd1091ff93956c70db3b2
racksoup <rack.connor1@gmail.com>
2024-07-13 04:54:39 -0400

    moddeathboxscreen textures aligned

--- MDK-main/src/main/java/com/devmod/ui/ModDeathBoxScreen.java
+++ MDK-main/src/main/java/com/devmod/ui/ModDeathBoxScreen.java
@@ -1,5 +1,6 @@
 package com.devmod.ui;
 
+import com.devmod.DevMod;
 import com.devmod.menus.ModDeathBoxMenu;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
@@ -8,17 +9,33 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Inventory;
 
 public class ModDeathBoxScreen extends AbstractContainerScreen<ModDeathBoxMenu> {
-    private static final ResourceLocation BG_TEXTURE = ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/container/generic_54.png");
+    private static final ResourceLocation CONTAINER_BACKGROUND = ResourceLocation.fromNamespaceAndPath("minecraft", "textures/gui/container/generic_54.png");
+    private int containerRows = 6;
 
     public ModDeathBoxScreen(ModDeathBoxMenu menu, Inventory inventory, Component component) {
         super(menu, inventory, component);
+    }
 
-        this.titleLabelX = 10;
-        this.inventoryLabelX = 10;
+    @Override
+    protected void init() {
+        super.init();
+        this.titleLabelX = 8;
+        this.inventoryLabelX = 8;
+        this.inventoryLabelY = 18 * (this.containerRows + 1) + 2;
+        this.topPos = (this.height - this.imageHeight) / 4 - 6;
+        DevMod.LOGGER.info("topPos: " + topPos + ", height: " + this.height + ", imageHeight: " + this.imageHeight);
+    }
+
+    @Override
+    public void render(GuiGraphics pGuiGraphics, int pMouseX, int pMouseY, float pPartialTick) {
+        super.render(pGuiGraphics, pMouseX, pMouseY, pPartialTick);
+        this.renderTooltip(pGuiGraphics, pMouseX, pMouseY);
     }
 
     @Override
     protected void renderBg(GuiGraphics pGuiGraphics, float pPartialTick, int pMouseX, int pMouseY) {
-        pGuiGraphics.blit(BG_TEXTURE, leftPos, topPos, 0, 0, imageWidth, imageHeight);
+        int i = (this.width - this.imageWidth) / 2;
+        pGuiGraphics.blit(CONTAINER_BACKGROUND, i, topPos, 0, 0, this.imageWidth, this.containerRows * 18 + 17);
+        pGuiGraphics.blit(CONTAINER_BACKGROUND, i, topPos + this.containerRows * 18 + 17, 0, 126, this.imageWidth, 96);
     }
 }
